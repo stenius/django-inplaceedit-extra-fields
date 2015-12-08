84cb8483f587cf322707333e33f8424baf433b16
Paul Stenius <stenius@users.noreply.github.com>
2015-12-08 14:07:30 -0600

    change location for django 1.9

--- inplaceeditform_extra_fields/widgets.py
+++ inplaceeditform_extra_fields/widgets.py
@@ -17,7 +17,7 @@ import sys
 
 from django.conf import settings
 from django.forms import widgets
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
